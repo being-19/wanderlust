fe4e91d3fe6e5453f3cf0fad381ef8cacce49203
being-19 <najilhossain539@gmail.com>
2026-07-11 11:54:57 +0530

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js")
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+};
+
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+    console.log(err);
+    });
+
+const intiDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: "68d6edf699a2b996771c977f"}));
+    await Listing.insertMany(initData.data);
+    console.log("data was intialized");
+};
+
+intiDB();
